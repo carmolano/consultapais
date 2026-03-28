e5e8ff4476423a399600dc47081b242f3474e4a6
camolano <cmolanom@unicartagena.edu.co>
2026-03-27 23:32:04 -0500

    feat(domain) implementacion de excepcion para el nombre de usuario
    se aplica esta descripcion para el metodo de name exception

---
+++ CRUD-USUARIOS/DOMAIN/Exeptions/InvalidUserNameExeption.php
@@ -0,0 +1,14 @@
+<?php
+
+class InvalidUserNameExeption extends InvalidArgumentException
+ {
+    public static function becauseValueIsEmpty()
+    {
+        return new  self('el nombre del usuario no puede estar vacio.');
+    }
+
+    public static function becauselenghtIsTooShort($min)
+    {
+        return new self('el nombre del usuario debe tener al menos' .$min.'caracteres.');
+    }
+ }
