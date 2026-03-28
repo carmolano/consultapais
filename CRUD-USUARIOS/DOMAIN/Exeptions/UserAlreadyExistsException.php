066be34702051fbf58d4e9f0573e3d07d4567f01
camolano <cmolanom@unicartagena.edu.co>
2026-03-27 23:36:30 -0500

    feat(domain) se agrega la exception para usario duplicados
    usuarios duplicados

---
+++ CRUD-USUARIOS/DOMAIN/Exeptions/UserAlreadyExistsException.php
@@ -0,0 +1,10 @@
+<?php
+
+class UserAlreadyExistsException extends DomainException
+{
+     public static function becauseEmailAlreadyExists($email)
+     {
+        return  new self('Ya existe un usuario con el email:'.$email);
+          }
+     
+     }
