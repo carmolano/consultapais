491f075d3fdbc63add98509da665ed51f83179e8
camolano <cmolanom@unicartagena.edu.co>
2026-04-02 11:22:48 -0500

    feat(domain) usuario existente con ese nombre email
    esto quiere decir que el usuario ya existe y que debe validar su email

--- CRUD-USUARIOS/DOMAIN/Exeptions/UserAlreadyExistsException.php
+++ CRUD-USUARIOS/DOMAIN/Exeptions/UserAlreadyExistsException.php
@@ -4,7 +4,7 @@ class UserAlreadyExistsException extends DomainException
 {
      public static function becauseEmailAlreadyExists($email)
      {
-        return  new self('Ya existe un usuario con el email:'.$email);
+        return  new self('Ya existe un usuario con el email: '.$email);
           }
      
      }
