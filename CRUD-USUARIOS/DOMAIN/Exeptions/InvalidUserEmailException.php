b4662efc1fa9504a5ab57284feebf80c589a78f5
camolano <cmolanom@unicartagena.edu.co>
2026-04-02 10:37:59 -0500

    feat (domain)EmailExceptions
    en esta excepcion el usuario se le informa que el email esta vacio , o esta mal escrito

---
+++ CRUD-USUARIOS/DOMAIN/Exeptions/InvalidUserEmailException.php
@@ -0,0 +1,20 @@
+<?php
+
+
+
+class InvalidUserEmailException extends InvalidArgumentException
+
+{
+     public  static function becauseFormatIsInvalid($email)
+     {
+       return new self('El formato del mail es invalido: ' .$email);
+
+
+     }
+
+     public static function becauseValuesIsEmpity()
+     {
+        return new self('el email del usuario no puede estar vacio.');
+     }
+
+}
