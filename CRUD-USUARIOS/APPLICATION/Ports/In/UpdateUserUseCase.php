1b3aa978764d307cf51acaeb4eeba064bcb9cc30
camolano <cmolanom@unicartagena.edu.co>
2026-04-04 19:58:30 -0500

    feat(application)puerto de entrada
    puerto de entrada applications

---
+++ CRUD-USUARIOS/APPLICATION/Ports/In/UpdateUserUseCase.php
@@ -0,0 +1,13 @@
+<?php
+
+
+declare(strict_types=1);
+
+
+require_once __DIR__ . '/../../Services/Dto/Commands/UpdateUserCommand.php'; 
+require_once __DIR__ . '/../../../Domain/Models/UserModel.php';
+
+interface UpdateUserUseCase
+{
+    public function xecute(UpdateUserCommand $command): UserModel;
+}
