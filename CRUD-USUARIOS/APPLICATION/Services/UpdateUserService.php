3df95b72cf447023515fc27d26ca5895a6cd40be
camolano <cmolanom@unicartagena.edu.co>
2026-04-04 20:22:12 -0500

    feat(application) gestion cambios
    este gestiona cambios parciales en un usuario existente

---
+++ CRUD-USUARIOS/APPLICATION/Services/UpdateUserService.php
@@ -0,0 +1,92 @@
+<?php
+
+declare(strict_types=1);
+
+require_once __DIR__ . '/../Ports/In/UpdateUserUseCase.php'; 
+require_once __DIR__ . '/../Ports/Out/UpdateUserPort.php'; 
+require_once __DIR__ . '/../Ports/Out/GetUserByIdPort.php'; 
+require_once __DIR__ . '/../Ports/Out/GetUserByEmailPort.php'; 
+require_once __DIR__ . '/Mappers/UserApplicationMapper.php'; 
+require_once __DIR__ . '/../../Domain/Exceptions/UserAlreadyExistsException.php'; 
+require_once __DIR__ . '/../../Domain/Exceptions/UserNotFoundException.php'; 
+require_once __DIR__ . '/../../Domain/ValueObjects/UserId.php'; 
+require_once __DIR__ . '/../../Domain/ValueObjects/UserEmail.php';
+
+
+
+
+final class UpdateUserService implements UpdateUserUseCase
+{
+    private UpdateUserPort $update_user_port;
+    private GetUserByIdPort $getUserByIdPort;
+    private GetUserByEmailPort $getUserByIdPort;
+
+
+    public function __construct(
+        UpdateUserPort $updateUserPort,
+        GetUserByIdOirt $getUserByIdPort
+
+
+    )  {
+            $this->updateUserPort      =  $update_user_port;
+            $this->getUserByIdPort     =  $getUserByIdPort;
+            $this->getUserByEmailPort  =  $getUserByEmailPort;
+
+
+    }   
+    
+           public function execute(UpdateUserCommand $command): UserModel
+
+           {
+              $userId        = new UserId($command->getId());
+              $currentUser   = $this->getUserByIdPort->getById($userId);
+
+
+              if($currentUser === null) {
+                throw UserNotFoundException::becauseIdWasNotFound($userId->value());
+
+              }
+
+                  $newEmail             =  new UserEmail($command->getEmail());
+                  $userWithSaneEmail      = $this->getUserByEmailPort->getByEmail($newEmail);
+
+                  if ($userWithSameEmail !== null && !$userWithSameEmail->id()->equals($userId)) { 
+                    throw UserAlreadyExistsException::becauseEmailAlreadyExists($newEmail->value());
+
+                  }
+
+                        $userToUpdate = UserApplicationMapper::fromUpdateCommandToModel($command); 
+                        
+                        return $this->updateUserPort->update($userToUpdate);
+
+
+           }
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
