cb32e63c8bf0e74679dc6d5c4231897d23b179ba
camolano <cmolanom@unicartagena.edu.co>
2026-04-04 21:50:31 -0500

    feat(applications)quieries
    estos representan una peticion de informacion sin alterar nada

---
+++ CRUD-USUARIOS/APPLICATION/Services/Dto/Commands/Queries/GetUserByIdQuery.php
@@ -0,0 +1,20 @@
+<?php
+
+
+declare(strict_types=1);
+
+
+final class GetUserByIdQuery
+
+
+{
+      private string $id;
+
+      public function __construct(string $id)
+          {
+            $this->id  = trim($id);
+          }
+  
+public function getid(): string {return $this->id;}
+
+}
