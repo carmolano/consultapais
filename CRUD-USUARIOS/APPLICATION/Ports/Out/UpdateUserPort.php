178a4c44e9c15e765c042565a7873c8e376b5053
camolano <cmolanom@unicartagena.edu.co>
2026-04-04 20:13:51 -0500

    feat(application) puerto de salida
    salida

---
+++ CRUD-USUARIOS/APPLICATION/Ports/Out/UpdateUserPort.php
@@ -0,0 +1,16 @@
+<?php
+
+
+
+declare(strict_types=1);
+
+require_once __DIR__ . '/../../../Domain/Models/UserModel.php';
+
+
+interface UpdateUserPort
+
+{
+    public function update(UserModel $user): UserModel;
+    
+    
+}
