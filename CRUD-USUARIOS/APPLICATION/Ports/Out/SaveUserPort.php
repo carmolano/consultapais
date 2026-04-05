f01e7408ffc8d374b07d58ca425930249cb71bc5
camolano <cmolanom@unicartagena.edu.co>
2026-04-04 20:12:57 -0500

    feat(application)puerto de salida
    puerto salida

---
+++ CRUD-USUARIOS/APPLICATION/Ports/Out/SaveUserPort.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+
+require_once __DIR__ . '/../../../Domain/Models/UserModel.php';
+
+interface SaveUserPort
+
+{
+
+      public function save(UserModel $user): UserModel;
+
+}
+
